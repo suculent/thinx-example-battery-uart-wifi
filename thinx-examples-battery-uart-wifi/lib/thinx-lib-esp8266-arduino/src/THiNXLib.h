64970ec754272334f3130df4c2b2db59197e4a21
Matěj Sychra <suculent@me.com>
2017-11-08 22:14:29 +0100

    updated library

--- thinx-examples-battery-uart-wifi/lib/thinx-lib-esp8266-arduino/src/THiNXLib.h
+++ thinx-examples-battery-uart-wifi/lib/thinx-lib-esp8266-arduino/src/THiNXLib.h
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 
+#ifndef VERSION
+#define VERSION "2.0.139"
+#endif
+
 #define __DEBUG__
 #define __DEBUG_JSON__
 
@@ -27,43 +31,47 @@
 #include <PubSubClient.h>
 
 #ifndef THX_REVISION
-  #ifdef THINX_FIRMWARE_VERSION_SHORT
-    #define THX_REVISION THINX_FIRMWARE_VERSION_SHORT
-  #endif
+#ifdef THINX_FIRMWARE_VERSION_SHORT
+#define THX_REVISION THINX_FIRMWARE_VERSION_SHORT
+#endif
 #endif
 
 class THiNX {
 
-  public:
+public:
 
-    #ifdef __USE_WIFI_MANAGER__
-        static WiFiManagerParameter *api_key_param;
-        static WiFiManagerParameter *owner_param;
-        static int should_save_config;                // after autoconnect, may provide new API Key
-        static void saveConfigCallback();
-    #endif
+    static double latitude;
+    static double longitude;
+    static String statusString;
+
+#ifdef __USE_WIFI_MANAGER__
+    static WiFiManagerParameter *api_key_param;
+    static WiFiManagerParameter *owner_param;
+    static int should_save_config;                // after autoconnect, may provide new API Key
+    static void saveConfigCallback();
+#endif
 
     THiNX();
     THiNX(const char *, const char *); // (const char * __apikey, const char * __owner_id)
     THiNX(const char *);  // (const char * __apikey)
 
     enum payload_type {
-      Unknown = 0,
-      UPDATE = 1,		                         // Firmware Update Response Payload
-      REGISTRATION = 2,		                   // Registration Response Payload
-      NOTIFICATION = 3,                      // Notification/Interaction Response Payload
-      CONFIGURATION = 4,                     // Environment variables update
-      Reserved = 255,		                     // Reserved
+        Unknown = 0,
+        UPDATE = 1,                                 // Firmware Update Response Payload
+        REGISTRATION = 2,                           // Registration Response Payload
+        NOTIFICATION = 3,                      // Notification/Interaction Response Payload
+        CONFIGURATION = 4,                     // Environment variables update
+        Reserved = 255,                             // Reserved
     };
 
     enum phase {
-      INIT = 0,
-      CONNECT_WIFI = 1,
-      CONNECT_API = 2,
-      CONNECT_MQTT = 3,
-      CHECKIN_MQTT = 4,
-      FINALIZE = 5,
-      COMPLETED = 6
+        INIT = 0,
+        CONNECT_WIFI = 1,
+        CONNECT_API = 2,
+        CONNECT_MQTT = 3,
+        CHECKIN_MQTT = 4,
+        FINALIZE = 5,
+        COMPLETED = 6
     };
 
     phase thinx_phase;
@@ -103,9 +111,6 @@ class THiNX {
     char* thinx_owner;
     char* thinx_udid;
 
-    static double latitude;
-    static double longitude;
-
     void setPushConfigCallback( void (*func)(String) );
     void setFinalizeCallback( void (*func)(void) );
 
@@ -118,105 +123,103 @@ class THiNX {
     void publishStatus(String);               // send String to status channel
     void publish(String, String, bool);       // send String to any channel, optinally with retain
 
-    private:
+    void setStatus(String);
 
-      bool connected;                         // WiFi connected in station mode
+private:
 
-      static char* thinx_api_key;
-      static char* thinx_owner_key;
+    bool connected;                         // WiFi connected in station mode
 
-      //
-      // Build-specific constants (override for Arduino IDE which does not set any Environments like PlatformIO)
-      //
+    static char* thinx_api_key;
+    static char* thinx_owner_key;
 
-      #ifdef THX_REVISION
-        const char* thx_revision = strdup(String(THX_REVISION).c_str());
-      #else
-        const char* thx_revision = "revision";
-      #endif
+    //
+    // Build-specific constants (override for Arduino IDE which does not set any Environments like PlatformIO)
+    //
 
-      #ifdef THINX_COMMIT_ID
-        const char* commit_id = strdup(THINX_COMMIT_ID);
-      #else
-        const char* commit_id = "commit-id";
-      #endif
-
-      #ifdef THINX_FIRMWARE_VERSION_SHORT
-        const char* firmware_version_short = THINX_FIRMWARE_VERSION_SHORT;
-      #else
-        #ifdef THX_REVISION
-          const char* firmware_version_short = strdup(String(THX_REVISION).c_str());
-        #else
-          const char* firmware_version_short = "123";
-        #endif
-      #endif
-
-      //
-      // THiNXLib
-      //
-
-      void configCallback();
-
-      // WiFi Manager
-      WiFiClient thx_wifi_client;
-      int status;                             // global WiFi status
-      bool once;                              // once token for initialization
-
-      // THiNX API
-      static char thx_api_key[65];            // static due to accesibility to WiFiManager
-      static char thx_owner_key[65];          // static due to accesibility to WiFiManager
-
-      char mac_string[17];
-      const char * thinx_mac();
-
-#ifndef __USE_SPIFFS__
-      char json_info[512] = {0};           // statically allocated to prevent fragmentation (?)
+#ifdef THX_REVISION
+    const char* thx_revision = strdup(String(THX_REVISION).c_str());
+#else
+    const char* thx_revision = "0";
 #endif
 
-      String json_output;
+#ifdef THX_COMMIT_ID
+    const char* commit_id = strdup(THX_COMMIT_ID);
+#else
+    const char* commit_id = "unknown";
+#endif
 
-      // In order of appearance
-      bool fsck();                            // check filesystem if using SPIFFS
-      void connect();                         // start the connect loop
-      void connect_wifi();                    // start connecting
-      void checkin();                         // checkin when connected
-      void senddata(String);
-      void parse(String);
-      void update_and_reboot(String);
+#ifdef THX_REVISION
+    const char* firmware_version_short = strdup(String(THX_REVISION).c_str());
+#else
+    const char* firmware_version_short = VERSION;
+#endif
 
-      // MQTT
-      bool start_mqtt();                      // connect to broker and subscribe
-      int mqtt_result;                       // success or failure on connection
-      int mqtt_connected;                    // success or failure on subscription
-      String mqtt_payload;                    // mqtt_payload store for parsing
-      int last_mqtt_reconnect;                // interval
-      int performed_mqtt_checkin;              // one-time flag
-      int all_done;                              // finalize flag
+    //
+    // THiNXLib
+    //
 
-      void (*_config_callback)(String) = NULL;  // Called when server pushes new environment vars using MQTT
+    void configCallback();
 
-      // Data Storage
-      void import_build_time_constants();     // sets variables from thinx.h file
-      void save_device_info();                // saves variables to SPIFFS or EEPROM
-      void restore_device_info();             // reads variables from SPIFFS or EEPROM
-      void deviceInfo();                    // TODO: Refactor to C-string
+    // WiFi Manager
+    WiFiClient thx_wifi_client;
+    int status;                             // global WiFi status
+    bool once;                              // once token for initialization
 
-      // Updates
-      void notify_on_successful_update();     // send a MQTT notification back to Web UI
+    // THiNX API
+    static char thx_api_key[65];            // static due to accesibility to WiFiManager
+    static char thx_owner_key[65];          // static due to accesibility to WiFiManager
 
-      // Event Queue / States
-      int mqtt_started;
-      bool complete;
-      void evt_save_api_key();
+    char mac_string[17];
+    const char * thinx_mac();
 
-      // Finalize
-      void (*_finalize_callback)(void) = NULL;
-      void finalize();                        // Complete the checkin, schedule, callback...
+#ifndef __USE_SPIFFS__
+    char json_info[512] = {0};           // statically allocated to prevent fragmentation (?)
+#endif
 
-      // Local WiFi Impl
-      bool wifi_wait_for_connect;
-      unsigned long wifi_wait_start;
-      unsigned long wifi_wait_timeout;
-      int wifi_retry;
-      uint8_t wifi_status;
+    String json_output;
+
+    // In order of appearance
+    bool fsck();                            // check filesystem if using SPIFFS
+    void connect();                         // start the connect loop
+    void connect_wifi();                    // start connecting
+    void checkin();                         // checkin when connected
+    void senddata(String);
+    void parse(String);
+    void update_and_reboot(String);
+
+    // MQTT
+    bool start_mqtt();                      // connect to broker and subscribe
+    int mqtt_result;                       // success or failure on connection
+    int mqtt_connected;                    // success or failure on subscription
+    String mqtt_payload;                    // mqtt_payload store for parsing
+    int last_mqtt_reconnect;                // interval
+    int performed_mqtt_checkin;              // one-time flag
+    int all_done;                              // finalize flag
+
+    void (*_config_callback)(String) = NULL;  // Called when server pushes new environment vars using MQTT
+
+    // Data Storage
+    void import_build_time_constants();     // sets variables from thinx.h file
+    void save_device_info();                // saves variables to SPIFFS or EEPROM
+    void restore_device_info();             // reads variables from SPIFFS or EEPROM
+    void deviceInfo();                    // TODO: Refactor to C-string
+
+    // Updates
+    void notify_on_successful_update();     // send a MQTT notification back to Web UI
+
+    // Event Queue / States
+    int mqtt_started;
+    bool complete;
+    void evt_save_api_key();
+
+    // Finalize
+    void (*_finalize_callback)(void) = NULL;
+    void finalize();                        // Complete the checkin, schedule, callback...
+
+    // Local WiFi Impl
+    bool wifi_wait_for_connect;
+    unsigned long wifi_wait_start;
+    unsigned long wifi_wait_timeout;
+    int wifi_retry;
+    uint8_t wifi_status;
 };
